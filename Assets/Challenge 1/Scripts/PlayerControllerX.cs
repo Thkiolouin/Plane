e143bebb365799263ca88520eb6298462c073b43
Loizos Kallinos <kallinos.loizos@gmail.com>
2024-04-19 11:46:11 +0300

    All Assets

---
+++ Assets/Challenge 1/Scripts/PlayerControllerX.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public float speed;
+    public float rotationSpeed;
+    public float verticalInput;
+    public float horizontalInput;
+    
+    
+    void FixedUpdate()
+    {
+        // get the user's vertical input
+        verticalInput = Input.GetAxis("Vertical");
+        horizontalInput = Input.GetAxis("Horizontal");
+        
+        
+        
+        // move the plane forward at a constant rate
+        transform.Translate(Vector3.forward * speed);
+
+        Debug.Log(horizontalInput);
+
+        if (horizontalInput <= -0.5f)
+        {
+            // tilt the plane up/down based on up/down arrow keys
+            transform.Rotate(Vector3.down * rotationSpeed * Time.deltaTime);
+        }
+        if (horizontalInput >= 0.5f)
+        {
+            // tilt the plane up/down based on up/down arrow keys
+            transform.Rotate(Vector3.up * rotationSpeed * Time.deltaTime);
+        }
+    }
+}
